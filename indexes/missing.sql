2dd9da78f6009f0519b939915d4cbc481d8d188f
unknown <mufford@LTDV-MUFFO.jomax.paholdings.com>
2014-04-08 13:05:18 -0500

    Initial commit to GitHub

---
+++ indexes/missing.sql
@@ -0,0 +1,55 @@
+/**********************************************************************************************************
+
+    NAME:           missing.sql
+
+    SYNOPSIS:       Displays potential missing indexes for a given database. Adding the indexes via the
+                    provided CREATE scripts may improve server performance. 
+
+    DEPENDENCIES:   The following dependencies are required to execute this script:
+                    - SQL Server 2005 or newer
+
+    AUTHOR:         Michelle Ufford, http://sqlfool.com
+    
+    CREATED:        2014-04-08
+    
+    VERSION:        1.0
+
+    LICENSE:        Apache License v2
+
+ ---------------------------------------------------------------------------------------------------------
+ --  DATE       VERSION     AUTHOR                  DESCRIPTION                                        --
+ ---------------------------------------------------------------------------------------------------------
+     20140408   1.0         Michelle Ufford         Open Sourced on GitHub
+**********************************************************************************************************/
+
+SELECT 
+      t.name AS 'affected_table'
+    , 'CREATE NONCLUSTERED INDEX IX_' + t.name + '_missing_'
+        + CONVERT(CHAR(8), GETDATE(), 112) + '_'
+        + CAST(ROW_NUMBER() OVER (PARTITION BY t.name 
+            ORDER BY CAST((ddmigs.user_seeks + ddmigs.user_scans) 
+                * ddmigs.avg_user_impact AS BIGINT) DESC) AS VARCHAR(3))
+        + ' ON ' + ddmid.statement 
+        + ' (' + ISNULL(ddmid.equality_columns,'') 
+        + CASE WHEN ddmid.equality_columns IS NOT NULL 
+            AND ddmid.inequality_columns IS NOT NULL THEN ',' 
+                ELSE '' END 
+        + ISNULL(ddmid.inequality_columns, '')
+        + ')' 
+        + ISNULL(' INCLUDE (' + ddmid.included_columns + ');', ';'
+        ) AS sql_statement
+    , ddmigs.user_seeks
+    , ddmigs.user_scans
+    , CAST((ddmigs.user_seeks + ddmigs.user_scans) 
+        * ddmigs.avg_user_impact AS BIGINT) AS 'est_impact'
+    , ddmigs.last_user_seek
+FROM sys.dm_db_missing_index_groups                                 AS ddmig
+INNER JOIN sys.dm_db_missing_index_group_stats                      AS ddmigs
+    ON ddmigs.group_handle = ddmig.index_group_handle
+INNER JOIN sys.dm_db_missing_index_details                          AS ddmid 
+    ON ddmig.index_handle = ddmid.index_handle
+INNER JOIN sys.tables                                               AS t
+    ON ddmid.[object_id] = t.[object_id]
+WHERE ddmid.database_id  =  DB_ID()                                                             ----> by default, only examines the current database
+  AND CAST((ddmigs.user_seeks + ddmigs.user_scans) * ddmigs.avg_user_impact AS BIGINT)  >  100  ----> 100 is a starting point; update value as appropriate
+ORDER BY CAST((ddmigs.user_seeks + ddmigs.user_scans) * ddmigs.avg_user_impact AS BIGINT) DESC;
---
+++ indexes/unused.sql
@@ -0,0 +1,85 @@
+/**********************************************************************************************************
+
+    NAME:           unused.sql
+
+    SYNOPSIS:       Displays potential unused indexes for the current database. Dropping these indexes 
+                    may improve database performance. These statistics are reset each time the server 
+                    is rebooted, so make sure to review the [sqlserver_start_time] value to ensure the 
+                    statistics are captured for a meaningful time period.
+
+    DEPENDENCIES:   The following dependencies are required to execute this script:
+                    - SQL Server 2005 or newer
+
+    AUTHOR:         Michelle Ufford, http://sqlfool.com
+    
+    CREATED:        2014-04-08
+    
+    VERSION:        1.0
+
+    LICENSE:        Apache License v2
+
+ ---------------------------------------------------------------------------------------------------------
+ --  DATE       VERSION     AUTHOR                  DESCRIPTION                                        --
+ ---------------------------------------------------------------------------------------------------------
+     20140408   1.0         Michelle Ufford         Open Sourced on GitHub
+**********************************************************************************************************/
+
+
+SELECT sqlserver_start_time FROM sys.dm_os_sys_info;
+
+DECLARE @dbid INT
+    , @dbName VARCHAR(100);
+
+SELECT @dbid = DB_ID()
+    , @dbName = DB_NAME();
+
+WITH partitionCTE (object_id, index_id, row_count, partition_count) 
+AS
+(
+    SELECT [object_id]
+        , index_id
+        , SUM([rows]) AS 'row_count'
+        , COUNT(partition_id) AS 'partition_count'
+    FROM sys.partitions
+    GROUP BY [object_id]
+        , index_id
+)
+
+SELECT OBJECT_NAME(i.[object_id]) AS objectName
+        , i.name
+        , CASE 
+            WHEN i.is_unique = 1 
+                THEN 'UNIQUE ' 
+            ELSE '' 
+          END + i.type_desc AS 'indexType'
+        , ddius.user_seeks
+        , ddius.user_scans
+        , ddius.user_lookups
+        , ddius.user_updates
+        , cte.row_count
+        , CASE WHEN partition_count > 1 THEN 'yes' 
+            ELSE 'no' END AS 'partitioned?'
+        , CASE 
+            WHEN i.type = 2 AND i.is_unique = 0
+                THEN 'Drop Index ' + i.name 
+                    + ' On ' + @dbName 
+                    + '.dbo.' + OBJECT_NAME(ddius.[object_id]) + ';'
+            WHEN i.type = 2 AND i.is_unique = 1
+                THEN 'Alter Table ' + @dbName 
+                    + '.dbo.' + OBJECT_NAME(ddius.[object_ID]) 
+                    + ' Drop Constraint ' + i.name + ';'
+            ELSE '' 
+          END AS 'SQL_DropStatement'
+FROM sys.indexes                                                        AS i
+INNER JOIN sys.dm_db_index_usage_stats                                  AS ddius
+    ON i.object_id = ddius.object_id
+        AND i.index_id = ddius.index_id
+INNER JOIN partitionCTE                                                 AS cte
+    ON i.object_id = cte.object_id
+        AND i.index_id = cte.index_id
+WHERE ddius.database_id = @dbid
+    AND i.type = 2                                                      ----> retrieve nonclustered indexes only
+    AND i.is_unique = 0                                                 ----> ignore unique indexes, we'll assume they're serving a necessary business use
+    AND (ddius.user_seeks + ddius.user_scans + ddius.user_lookups) = 0  ----> starting point, update this value as needed; 0 retrieves completely unused indexes
+ORDER BY user_updates DESC;
+
